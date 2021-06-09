593c6872c7b286594106c871af4425312efbc666
Alberto Villarejo <a.villarejo19@icloud.com>
2021-06-09 14:21:14 -0500

    setup folder structure and react-navigation

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-#Recipes App
+# Recipes App
 
 > react native food recipe app
 
---
+++ screens/index.js
@@ -0,0 +1,9 @@
+import Home from './Home'
+import Login from './Login'
+import Recipe from './Recipe'
+
+export {
+  Home,
+  Login,
+  Recipe
+}
